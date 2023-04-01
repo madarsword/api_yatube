b3709c2621fb5b7a3b0a211c84a8e3df8a1909ae
al-shp <alshp@tutanota.com>
2023-04-01 17:57:08 +0200

    finish1

---
+++ yatube_api/api/serializers.py
@@ -0,0 +1,26 @@
+from posts.models import Comment, Group, Post
+from rest_framework import serializers
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(read_only=True, slug_field='username')
+
+    class Meta:
+        model = Post
+        fields = ('id', 'text', 'pub_date', 'author', 'image', 'group')
+
+
+class GroupSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Group
+        fields = ('id', 'title', 'slug', 'description')
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(read_only=True, slug_field='username')
+    post = serializers.PrimaryKeyRelatedField(read_only=True)
+    
+    class Meta:
+        model = Comment
+        fields = ('id', 'author', 'post', 'text', 'created')
